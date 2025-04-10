a3873bba2a35eb7c3a42041774ee81198fd21821
Emily Boarer <e@boarer.uk>
2025-04-10 01:07:17 +0100

    Updated graphics driver settings. Game perf still bad (but a bit better?)

--- hosts/Orchid/configuration.nix
+++ hosts/Orchid/configuration.nix
@@ -76,12 +76,43 @@
     kitty
     swww # wallpapers
     rofi-wayland # TODO what is this one too?
+
+    steam
+    nvtop
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  hardware.xone.enable = true; # support for the xbox controller USB dongle
+
+  # Configure Hardware settings to actually give decent performance when playing games.
+  # TODO move this config into hardware-configuration.nix?
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    forceFullCompositionPipeline = true; # Fixes tearing
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  # Enable OpenGL
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver
+    libvdpau-va-gl
+    vaapiIntel
+    vaapiVdpau
+    vulkan-tools
+    vulkan-validation-layers
   ];
 
 
-  # TODO: get steam working again (once got hyprland working, in case the WM situation is messing with things)
-  programs.steam = {
-    enable = true;
-  };
+  # hardware.opengl.driSupport = true; # No longer has any effect
+  hardware.pulseaudio.support32Bit = true;
+  boot.kernelParams = ["nvidia_drm.modeset=1"];
 
 }
